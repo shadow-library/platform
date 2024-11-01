2af69059883bb379ba2375b9f144ef5002ed9778
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-11-01 18:19:03 +0400

    feat: field decorator types (#3)

--- packages/class-schema/src/decorators/field.decorator.ts
+++ packages/class-schema/src/decorators/field.decorator.ts
@@ -7,14 +7,14 @@ import merge from 'deepmerge';
 /**
  * Importing user defined packages
  */
-import { JSONSchema } from '@lib/types';
+import { JSONArraySchema, JSONBasicSchema, JSONNumberSchema, JSONObjectSchema, JSONSchema, JSONStringSchema } from '@lib/types';
 import { DESIGN_TYPE_METADATA, FIELD_OPTIONS_METADATA, FIELD_TYPE_METADATA } from '@lib/constants';
 
 /**
  * Defining types
  */
 
-export type FieldType = Class<any> | [Class<any>];
+export type FieldType = Class<any> | Class<any>[];
 
 export type ReturnTypeFunc = (returns?: void) => FieldType;
 
@@ -25,7 +25,11 @@ export type FieldOptions = Partial<JSONSchema>;
  */
 
 export function Field(options?: FieldOptions): PropertyDecorator;
-export function Field(returnTypeFn?: ReturnTypeFunc, options?: FieldOptions): PropertyDecorator;
+export function Field(returnTypeFn: (returns?: void) => Class<String>, options?: Partial<JSONStringSchema>): PropertyDecorator;
+export function Field(returnTypeFn: (returns?: void) => Class<Number>, options?: Partial<JSONNumberSchema>): PropertyDecorator;
+export function Field(returnTypeFn: (returns?: void) => Class<Boolean>, options?: Partial<JSONBasicSchema>): PropertyDecorator;
+export function Field(returnTypeFn: (returns?: void) => Class<any>, options?: Partial<JSONObjectSchema>): PropertyDecorator;
+export function Field(returnTypeFn: (returns?: void) => Class<any>[], options?: Partial<JSONArraySchema>): PropertyDecorator;
 export function Field(typeOrOptions?: ReturnTypeFunc | FieldOptions, fieldOptions?: FieldOptions): PropertyDecorator {
   const isTypeFn = typeof typeOrOptions === 'function';
   const options = isTypeFn ? fieldOptions : typeOrOptions;
--- packages/class-schema/src/types.ts
+++ packages/class-schema/src/types.ts
@@ -10,48 +10,16 @@
  * Defining types
  */
 
+type SchemaWithOutType<T> = Omit<T, 'type'>;
+
 export type JSONSchemaType = 'string' | 'number' | 'boolean' | 'array' | 'object';
 
-export interface JSONSchema {
+export interface JSONBasicSchema {
   /** Basic schema properties */
   $schema?: string;
   $id?: string;
   $ref?: string;
   type?: JSONSchemaType | JSONSchemaType[];
-
-  /** Object-related properties */
-  properties?: Record<string, JSONSchema>;
-  required?: string[];
-  additionalProperties?: boolean | JSONSchema;
-  patternProperties?: Record<string, JSONSchema>;
-  dependencies?: Record<string, JSONSchema | string[]>;
-
-  /** Array-related properties */
-  items?: JSONSchema | JSONSchema[];
-  additionalItems?: boolean | JSONSchema;
-  minItems?: number;
-  maxItems?: number;
-  uniqueItems?: boolean;
-
-  /** String-related properties */
-  minLength?: number;
-  maxLength?: number;
-  pattern?: string;
-  format?: string;
-  enum?: any[];
-
-  /** Number-related properties */
-  minimum?: number;
-  maximum?: number;
-  exclusiveMinimum?: number;
-  exclusiveMaximum?: number;
-  multipleOf?: number;
-
-  /** Conditional subschemas */
-  allOf?: JSONSchema[];
-  anyOf?: JSONSchema[];
-  oneOf?: JSONSchema[];
-  not?: JSONSchema;
 
   /** Metadata and annotations */
   title?: string;
@@ -63,3 +31,54 @@ export interface JSONSchema {
   const?: any;
   definitions?: Record<string, JSONSchema>;
 }
+
+export interface JSONObjectSchema extends JSONBasicSchema {
+  type: 'object';
+  properties?: Record<string, JSONSchema>;
+  required?: string[];
+  additionalProperties?: boolean | JSONSchema;
+  patternProperties?: Record<string, JSONSchema>;
+  dependencies?: Record<string, JSONSchema | string[]>;
+}
+
+export interface JSONArraySchema extends JSONBasicSchema {
+  type: 'array';
+  items?: JSONSchema | JSONSchema[];
+  additionalItems?: boolean | JSONSchema;
+  minItems?: number;
+  maxItems?: number;
+  uniqueItems?: boolean;
+}
+
+export interface JSONStringSchema extends JSONBasicSchema {
+  type: 'string';
+  minLength?: number;
+  maxLength?: number;
+  pattern?: string;
+  format?: string;
+  enum?: any[];
+}
+
+export interface JSONNumberSchema extends JSONBasicSchema {
+  type: 'number';
+  minimum?: number;
+  maximum?: number;
+  exclusiveMinimum?: number;
+  exclusiveMaximum?: number;
+  multipleOf?: number;
+}
+
+export interface JSONConditionalSchema extends JSONBasicSchema {
+  allOf?: JSONSchema[];
+  anyOf?: JSONSchema[];
+  oneOf?: JSONSchema[];
+  not?: JSONSchema;
+}
+
+export interface JSONSchema
+  extends SchemaWithOutType<JSONBasicSchema>,
+    SchemaWithOutType<JSONObjectSchema>,
+    SchemaWithOutType<JSONArraySchema>,
+    SchemaWithOutType<JSONStringSchema>,
+    SchemaWithOutType<JSONNumberSchema>,
+    SchemaWithOutType<JSONConditionalSchema> {}
