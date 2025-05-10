6e1266a9c1ed800ba076062c805c8d97a4017725
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2025-05-10 12:13:45 +0400

    feat: limit depth in recursive types for dot notation (#11)

--- packages/common/src/interfaces/dot-notation.interface.ts
+++ packages/common/src/interfaces/dot-notation.interface.ts
@@ -14,6 +14,16 @@
  * Declaring the constants
  */
 
-export type DotNotation<T, Prefix extends string = ''> = {
-  [K in keyof T & string]: T[K] extends object ? (T[K] extends any[] ? Record<`${Prefix}${K}`, T[K]> : DotNotation<T[K], `${Prefix}${K}.`>) : Record<`${Prefix}${K}`, T[K]>;
-}[keyof T & string];
+type Prev = [never, 0, 1, 2, 3, 4, 5, ...0[]]; // supports depth up to 5
+
+export type DotNotation<T, Prefix extends string = '', D extends number = 5> = [D] extends [never]
+  ? unknown // stop if depth exceeded
+  : T extends object
+    ? {
+        [K in keyof T & string]: T[K] extends object
+          ? T[K] extends any[]
+            ? Record<`${Prefix}${K}`, T[K]>
+            : DotNotation<T[K], `${Prefix}${K}.`, Prev[D]>
+          : Record<`${Prefix}${K}`, T[K]>;
+      }[keyof T & string]
+    : never;
