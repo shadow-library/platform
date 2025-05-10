8cd8787d20742f01c46b4601bc942c98b77aeda9
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2025-05-10 11:56:31 +0400

    feat: export interfaces (#10)

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 export * from './classes';
 export * from './errors';
+export * from './interfaces';
 export * from './services';
 export * from './shorthands';
 export * from './utils';
