049dcfe684fee464d8401e470b1eaf2a7dbded1e
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2026-01-30 20:30:29 +0400

    chore: improve code quality and CI configuration (#67)

--- packages/class-schema/src/enum-type.ts
+++ packages/class-schema/src/enum-type.ts
@@ -14,6 +14,8 @@ import { EnumFieldSchema } from './interfaces';
  * Defining types
  */
 
+type ValidEnumType = 'string' | 'number';
+
 /**
  * Declaring the constants
  */
@@ -21,17 +23,19 @@ import { EnumFieldSchema } from './interfaces';
 export class EnumType<T extends string | number = string | number> {
   readonly id: string;
   readonly values: T[];
-  readonly type: 'string' | 'number';
+  readonly type: ValidEnumType;
   readonly options: EnumFieldSchema;
 
   private constructor(name: string, values: T[], options: EnumFieldSchema) {
+    if (values.length === 0) throw new InternalError('Enum must have at least one value');
+    const type = typeof values[0] as ValidEnumType;
+    const isValidEnum = values.every(v => typeof v === type);
+    if (!isValidEnum) throw new InternalError('All enum values must be of the same type');
+
     this.id = `class-schema:${name}-enum-${getCounterId()}`;
-    this.type = typeof values[0] === 'number' ? 'number' : 'string';
+    this.type = type;
     this.values = values;
     this.options = options;
-
-    const isValidEnum = values.every(v => typeof v === this.type);
-    if (!isValidEnum) throw new InternalError('All enum values must be of the same type');
   }
 
   static create<T extends string | number>(name: string, values: T[], options: EnumFieldSchema = {}): EnumType<T> {
