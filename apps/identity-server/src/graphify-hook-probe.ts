c82bc3418152a9b4e42db65312eabac43bcf6770
Leander Paul <leanderpaul97@outlook.com>
2026-08-02 13:36:46 +0400

    test(identity): add graphify hook probe

---
+++ apps/identity-server/src/graphify-hook-probe.ts
@@ -0,0 +1,21 @@
+/**
+ * Importing npm packages
+ */
+
+/**
+ * Importing user defined packages
+ */
+
+/**
+ * Defining types
+ */
+
+/**
+ * Declaring the constants
+ */
+
+export const GraphifyHookProbeSymbol = 'graphify-hook-probe';
+
+export function graphifyHookProbeFunction(): string {
+  return GraphifyHookProbeSymbol;
+}
